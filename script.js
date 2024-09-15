2b74bf371119023bcb0721fbabf816f18947cec8
Aslamarley <lawrenceozimana@gmail.com>
2024-09-15 12:07:27 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const apiKey = "1fda312d5815c05827c06c1fb56e499c";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-iocn");
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }else{
+        var data = await response.json();
+
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style. display = "none"
+}
+
+
+    }
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
